374bfc9a3c99c7b6e386ab9d05e2b4435c63a03f
Ravikanth45 <155633315+Ravikanth45@users.noreply.github.com>
2024-12-27 10:12:49 +0530

    Add files via upload

---
+++ method.java
@@ -0,0 +1,21 @@
+class calculaton1 {
+	void sum(int a,int b) {
+		System.out.println(a+b);
+	}
+	void sum(int a,int b,int c) {
+		System.out.println(a+b+c);
+	}
+	void sum(double a,double b) {
+		System.out.println(a*b);
+	}
+}
+public class method {
+	public static void main(Strig[] args) {
+		calculation1 ob=new calculation1();
+			ob.sum=(10,10,10);
+			ob.sum=(20,20);
+			ob.sum=(10.0,10.0);
+	}
+}
+			
+	
